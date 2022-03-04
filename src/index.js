ecad00ca79f32c0d5eabc78b539f854d210495ee
soohyun <bmy1320@gmail.com>
2022-03-05 00:08:28 +0900

    handgame_01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+        <h1 id="title">가위바위보</h1>
+        <button className="hand">가위</button>
+        <button className="hand">바위</button>
+        <button className="hand">보</button>
+    </>
+, document.getElementById('root'));
