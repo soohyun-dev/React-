1c51a54bcf213ee459842ec6f1133070b4a9721d
soohyun <bmy1320@gmail.com>
2022-03-07 23:25:34 +0900

    2022-03-07 handgame_05

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,10 +1,12 @@
 import HandIcon from "./HandIcon";
+import backgroundImg from './assets/purple.svg';
+import './HandButton.css';
 
 function HandButton({ value, onClick }) {
     const handleClick = () => onClick(value);
     return (
-        <button onClick={handleClick} > 
-            <HandIcon value={value}  /> 
+        <button className="HandButton" onClick={handleClick}> 
+            <HandIcon className="HandButton-icon" value={value} /> 
         </button>
     );
 }
